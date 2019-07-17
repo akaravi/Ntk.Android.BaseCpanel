0837b2d2a46ac8da838b26dfda220a7998cf5ba0
me <zsdg98@gmail.com>
2019-07-17 11:04:25 +0430

    Add entity.

--- api/src/main/java/ntk/base/api/core/entity/CoreGuide.java
+++ api/src/main/java/ntk/base/api/core/entity/CoreGuide.java
@@ -1,6 +1,37 @@
 package ntk.base.api.core.entity;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 import ntk.base.api.model.BaseModuleEntity;
 
 public class CoreGuide extends BaseModuleEntity {
+
+    @SerializedName("Title")
+    public String Title;
+
+    @SerializedName("BodyFa")
+    public String BodyFa;
+
+    @SerializedName("BodyEn")
+    public String BodyEn;
+
+    @SerializedName("BodyAr")
+    public String BodyAr;
+
+    @SerializedName("LinkFileIds")
+    public String LinkFileIds;
+
+    @SerializedName("LinkParentId")
+    public Long LinkParentId;
+
+    @SerializedName("virtual_Parent")
+    public CoreUserGroup virtual_Parent;
+
+    @SerializedName("Parent")
+    public CoreUserGroup Parent;
+
+    @SerializedName("Children")
+    public List<CoreUserGroup> Children;
 }
