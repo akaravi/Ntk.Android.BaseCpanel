c9e225d3898a3df2d121fa264fe9fa86516fde99
me <zsdg98@gmail.com>
2019-08-13 10:30:56 +0430

    Fix Bugs.

--- api/src/main/java/ntk/base/api/news/interfase/INewsContent.java
+++ api/src/main/java/ntk/base/api/news/interfase/INewsContent.java
@@ -12,12 +12,13 @@ import ntk.base.api.news.model.NewsCountRequest;
 import ntk.base.api.news.model.NewsExportFileRequest;
 import ntk.base.api.news.model.NewsGetAllRequest;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
-import retrofit2.http.HTTP;
 import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface INewsContent {
 
@@ -41,10 +42,10 @@ public interface INewsContent {
     @Headers({"content-type: application/json"})
     Observable<NewsContentResponse> Edit(@HeaderMap Map<String, String> headers, @Body NewsContentEditRequest request);
 
-    //    @DELETE("api/newsContent/delete/")
-    @HTTP(method = "DELETE", path = "api/newsContent/delete/", hasBody = true)
+    @DELETE("api/newsContent/delete/{request}")
+//    @HTTP(method = "DELETE", path = "api/newsContent/delete/", hasBody = true)
     @Headers({"content-type: application/json"})
-    Observable<NewsContentResponse> Delete(@HeaderMap Map<String, String> headers, @Body NewsContent request);
+    Observable<NewsContentResponse> Delete(@HeaderMap Map<String, String> headers, @Path("request") NewsContent request);
 
     @POST("api/newsContent/exportfile/")
     @Headers({"content-type: application/json"})
