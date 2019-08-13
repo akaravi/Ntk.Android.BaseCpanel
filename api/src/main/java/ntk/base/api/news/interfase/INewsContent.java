077e78aae57a1cf30a1b64bd1d5a04f68b62dcce
me <zsdg98@gmail.com>
2019-08-13 13:51:13 +0430

    Fix Bugs.

--- api/src/main/java/ntk/base/api/news/interfase/INewsContent.java
+++ api/src/main/java/ntk/base/api/news/interfase/INewsContent.java
@@ -12,13 +12,13 @@ import ntk.base.api.news.model.NewsCountRequest;
 import ntk.base.api.news.model.NewsExportFileRequest;
 import ntk.base.api.news.model.NewsGetAllRequest;
 import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.HeaderMap;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface INewsContent {
 
@@ -28,7 +28,7 @@ public interface INewsContent {
 
     @GET("api/newsContent/getviewmodel/")
     @Headers({"content-type: application/json"})
-    Observable<NewsContentResponse> GetViewModel(@HeaderMap Map<String, String> headers);
+    Observable<NewsContentResponse> GetViewModel(@HeaderMap Map<String, String> headers, @Query("") String id);
 
     @POST("api/newsContent/GetAllWithSimilarsId/")
     @Headers({"content-type: application/json"})
@@ -42,10 +42,9 @@ public interface INewsContent {
     @Headers({"content-type: application/json"})
     Observable<NewsContentResponse> Edit(@HeaderMap Map<String, String> headers, @Body NewsContentEditRequest request);
 
-    @DELETE("api/newsContent/delete/{request}")
-//    @HTTP(method = "DELETE", path = "api/newsContent/delete/", hasBody = true)
+    @HTTP(method = "DELETE", path = "api/newsContent/delete/", hasBody = true)
     @Headers({"content-type: application/json"})
-    Observable<NewsContentResponse> Delete(@HeaderMap Map<String, String> headers, @Path("request") NewsContent request);
+    Observable<NewsContentResponse> Delete(@HeaderMap Map<String, String> headers, @Body NewsContent request);
 
     @POST("api/newsContent/exportfile/")
     @Headers({"content-type: application/json"})
